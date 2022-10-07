a0f5d4a82e9d1250921864e27567583ad357ffb0
barreiro <lbbbarreiro@gmail.com>
2022-10-07 19:53:13 +0100

    Add feature to print the report periodically

--- agent/src/main/java/io/type/pollution/agent/Agent.java
+++ agent/src/main/java/io/type/pollution/agent/Agent.java
@@ -12,6 +12,8 @@ import net.bytebuddy.matcher.ElementMatcher;
 import net.bytebuddy.pool.TypePool;
 
 import java.lang.instrument.Instrumentation;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
@@ -20,43 +22,24 @@ public class Agent {
     private static final boolean ENABLE_STATISTICS_CLEANUP =  Boolean.getBoolean("io.type.pollution.cleanup");
     private static final boolean ENABLE_FULL_STACK_TRACES =  Boolean.getBoolean("io.type.pollution.full.traces");
 
+    private static final Long REPORT_INTERVAL = Long.getLong("io.type.pollution.report.interval");
+
     public static void premain(String agentArgs, Instrumentation inst) {
         if (ENABLE_FULL_STACK_TRACES) {
             TraceInstanceOf.startMetronome();
         }
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("--------------------------\nType Pollution Statistics:");
-            class MutableInt {
-                int rowId = 0;
-            }
-            MutableInt mutableInt = new MutableInt();
-            TraceInstanceOf.orderedSnapshot(ENABLE_STATISTICS_CLEANUP).forEach(snapshot -> {
-                mutableInt.rowId++;
-                System.out.println("--------------------------\n" + mutableInt.rowId + ":\t" + snapshot.clazz.getName());
-                System.out.println("Count:\t" + snapshot.updateCount + "\nTypes:");
-                for (Class seen : snapshot.seen) {
-                    System.out.println("\t" + seen.getName());
-                }
-                System.out.println("Traces:");
-                for (String stack : snapshot.topStackTraces) {
-                    System.out.println("\t" + stack);
-                }
-                if (ENABLE_FULL_STACK_TRACES) {
-                    summary.append("Full Traces:\n");
-                    int i = 1;
-                    for (StackTraceElement[] fullFrames : snapshot.fullStackFrames) {
-                        summary.append("\t--------------------------\n");
-                        i++;
-                        for (StackTraceElement frame : fullFrames) {
-                            summary.append("\t").append(frame).append('\n');
-                        }
-                    }
-                }
-            });
-            if (mutableInt.rowId > 0) {
-                System.out.println("--------------------------\n");
-            }
-        }));
+
+        if (REPORT_INTERVAL != null) {
+            Executors.newSingleThreadScheduledExecutor(r -> {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                t.setName("type-pollution-periodic-report");
+                return t;
+            }).scheduleWithFixedDelay(Agent::printReport, REPORT_INTERVAL, REPORT_INTERVAL, TimeUnit.SECONDS);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(Agent::printReport));
+
         final String[] agentArgsValues = agentArgs == null ? null : agentArgs.split(",");
         ElementMatcher.Junction<? super TypeDescription> acceptedTypes = any();
         if (agentArgsValues != null && agentArgsValues.length > 0) {
@@ -98,6 +81,40 @@ public class Agent {
                         return new ByteBuddyUtils.ByteBuddyTypePollutionClassVisitor(net.bytebuddy.jar.asm.Opcodes.ASM9, classVisitor);
                     }
                 })).installOn(inst);
+    }
+
+    private static void printReport() {
+        System.out.println("--------------------------\nType Pollution Statistics:");
+        class MutableInt {
+            int rowId = 0;
+        }
+        MutableInt mutableInt = new MutableInt();
+        TraceInstanceOf.orderedSnapshot(ENABLE_STATISTICS_CLEANUP).forEach(snapshot -> {
+            mutableInt.rowId++;
+            System.out.println("--------------------------\n" + mutableInt.rowId + ":\t" + snapshot.clazz.getName());
+            System.out.println("Count:\t" + snapshot.updateCount + "\nTypes:");
+            for (Class<?> seen : snapshot.seen) {
+                System.out.println("\t" + seen.getName());
+            }
+            System.out.println("Traces:");
+            for (String stack : snapshot.topStackTraces) {
+                System.out.println("\t" + stack);
+            }
+            if (ENABLE_FULL_STACK_TRACES) {
+                System.out.println("Full Traces:");
+                int i = 1;
+                for (StackTraceElement[] fullFrames : snapshot.fullStackFrames) {
+                    System.out.println("\t--------------------------");
+                    i++;
+                    for (StackTraceElement frame : fullFrames) {
+                        System.out.println("\t" + frame);
+                    }
+                }
+            }
+        });
+        if (mutableInt.rowId > 0) {
+            System.out.println("--------------------------\n");
+        }
     }
 }
 
